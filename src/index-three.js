03500285a62389c347a4b88c1ca18cc6c34e3bec
Marco Fugaro <marco.fugaro@gmail.com>
2022-05-23 10:10:48 +0200

    More complete three.js example

--- src/index-three.js
+++ src/index-three.js
@@ -1,3 +1,16 @@
 import * as THREE from 'three'
 
-console.log(new THREE.WebGLRenderer())
+const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+
+const scene = new THREE.Scene();
+
+const renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animation );
+document.body.appendChild( renderer.domElement );
+
+function animation( time ) {
+
+	renderer.render( scene, camera );
+
+}
