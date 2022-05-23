f8a728baba0fd67a7387c39cb4fc8b01fb910cea
Marco Fugaro <marco.fugaro@gmail.com>
2022-05-23 10:09:57 +0200

    Remove parcel

--- dist/index-parcel.js
+++
@@ -1,1 +0,0 @@
-(function () {function b(s,a){if(!(s instanceof a))throw new TypeError("Cannot call a class as a function")}var c=function s(){b(this,s)},d=function s(){b(this,s)};d.prototype.type="UnusedClass",console.log(new c);})();
